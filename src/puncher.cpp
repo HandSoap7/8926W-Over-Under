e5652a2ae0b4c691a1e3a3380f4b22bf6ee56d8d
HandSoap7 <handsoap71@gmail.com>
2023-06-18 13:02:04 -0500

    puncher code

--- src/puncher.cpp
+++ src/puncher.cpp
@@ -1,6 +1,39 @@
 
 #include "puncher.hpp"
+#include "pros/adi.hpp"
 #include "pros/motors.h"
+#include "pros/rotation.hpp"
 #include "pros/rtos.hpp"
 
-pros::Motor flywheel(9, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor puncher(9, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+
+pros::ADIDigitalIn puncher_limit('A');
+pros::ADIDigitalIn puncher_limit2('B');
+
+pros::Rotation puncher_rotation(9);
+
+
+void puncher_task(void* param) {
+  while (true) {
+    if (puncher_rotation.get_angle() < 200) {
+      puncher.move_velocity(200);
+    } else {
+      puncher.move_velocity(0);
+    }
+    pros::delay(10);
+  }
+}
+
+void puncher_init() {
+  pros::Task puncher_reload(puncher_task, NULL, "Puncher Task");
+}
+
+void puncher_fire() {
+  puncher.move_velocity(200);
+  pros::delay(1000);
+  puncher.move_velocity(0);
+}
+
+void puncher_stop() {
+  puncher.move_velocity(0);
+}
